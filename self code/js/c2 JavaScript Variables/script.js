15e6283d68ef53c78f6746a30564ba7885c70647
Alin sharma <143596874+Alinsharma@users.noreply.github.com>
2025-10-08 12:21:08 +0530

    Add files via upload

---
+++ self code/js/c2 JavaScript Variables/script.js
@@ -0,0 +1,29 @@
+console.log("hello i am alin")
+
+let a=4;
+a=a+1
+b=555
+console.log(a)
+console.log(b)
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o ={
+    name :"i am new",
+   " job code ": 56677,
+   " it is  nice" : true
+}
+console.log(o)
+o.sal="sigma a";
+console.log(o)
+
+o.saloododododod="siiiiiggggma opa";
+console.log(o)
---
+++ self code/js/c5 function/script.js
@@ -0,0 +1,34 @@
+// function ln(good){
+    
+//    console.log("i am" +good +" ii am good  i")
+//    console.log("i am" +good +" ii am good  i")
+//    console.log("i am "+good + "ii am good  i")
+//    console.log("i am " +good +" ii am good  i")
+
+    
+
+// }
+// ln("baad")
+// ln("durty")
+
+function sum( a,b,c=3){
+    console.log(a+b+c)
+
+}
+sum(6,7)
+sum(1,7)
+sum(6,8)
+sum(0,9)
+
+console.log("The sum of these numbers is: ",sum (6,7))
+console.log("The sum of these numbers is: ",sum(1,7))
+console.log("The sum of these numbers is: ",sum (0,9))
+
+
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+}
+
+func1(34);
+func1(66);
+func1(84);
